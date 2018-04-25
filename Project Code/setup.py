28adad190e0965217d60e9ac149704928adc7ea6
Wesley Tyler Lee <wtl5736@rit.edu>
2018-04-25 10:03:40 -0400

    Final Commit

--- Project Code/setup.py
+++ Project Code/setup.py
@@ -12,7 +12,7 @@ from os import *
 def setup():
 	system("clear")
 	system("echo Installing Necessary Dependencies to Run Crypto_Project.py")
-	system("pip3 install pyqt5 numpy")
+	system("pip3 install PyQt5 numpy")
 	system("echo '\nFinsihed Installing Dependencies...'")
 	system("echo To Run Crypto_Project.py: ")
 	system("echo '\t\tpython3 Crypto_Project.py\n'")
